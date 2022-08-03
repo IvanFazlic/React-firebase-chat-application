b3465f39adcd244155b7843dd965bde1d3c516ec
Ivan Fazlic <fazlic.ivan00@gmail.com>
2022-08-03 02:24:11 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from "react-firebase-hooks/firestore";
+import "firebase/firestore";
+firebase.initializeApp({
+  apiKey: "AIzaSyC1Q3rwpdWuNEoxMGcJC5A135mYSu46sGI",
+  authDomain: "application-7d349.firebaseapp.com",
+  projectId: "application-7d349",
+  storageBucket: "application-7d349.appspot.com",
+  messagingSenderId: "161663329944",
+  appId: "1:161663329944:web:5d35ec8eba9e757a5812ed",
+  measurementId: "G-QWF2KYRB0M"
+})
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Bibito inc.🤝</h1>
+        <SignOut />
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+      
     </div>
   );
+}
+
+function ChatRoom(){
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy("createAt")
+  const [messages] = useCollectionData(query, {idField:'id'})
+  const [formValue,setFormValue]= useState("")
+  const dummy=useRef()
+  const sendMessage = async(e)=>{
+    e.preventDefault()
+    const {uid,photoURL} = auth.currentUser;
+    await messagesRef.add({
+      text:formValue,
+      createAt:firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+    setFormValue("")
+    dummy.current.scrollIntoView({behavior:'smooth'})
+  }
+  return(
+    <>
+      <main>
+        {messages && messages.map(msg=><ChatMessage key={msg.id} message={msg}/>)}
+        <div ref={dummy}></div>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={e=>setFormValue(e.target.value)}/>
+        <button type="submit">➜</button>
+      </form>
+    </>
+  )
+}
+
+function ChatMessage(props){
+  const {text,uid,photoURL}=props.message;
+  const messageClass=uid === auth.currentUser.uid ? 'sent' : 'received';
+   return (<div className={`message ${messageClass}`}>
+    <img src={photoURL} alt={photoURL}/>
+      <p>
+        {text}
+      </p>
+    </div>)
+}
+
+function SignOut(){
+  return auth.currentUser && (
+     <button onClick={()=> auth.signOut()}>Sign Out</button>
+  )
+
+}
+
+function SignIn(){
+  const signInWithGoogle = ()=>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
 }
 
 export default App;
